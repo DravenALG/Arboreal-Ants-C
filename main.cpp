9fd43b0e9b7fa50354541384c18e3c91596b573d
Xiao-Ming Wu <84899966+DravenALG@users.noreply.github.com>
2023-04-24 18:43:18 +0800

    Finish

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ const double inc_rate = 1; // factor by which flow increases at each time step
 const int max_iter = 1000;
 const double start_flow = 1;
 const double end_flow = 1;
+const double decay = 1;
 
 
 /* Load graph */
@@ -33,13 +34,13 @@ double **load_graph()
 }
 
 /* Init pheromone */
-double **init_pher(){
+double **init_pher(double ** graph){
 	double **init_pher = new double*[number_of_vertics];
 	for (int i = 0; i < number_of_vertics; i++)
 	{
 		init_pher[i] = new double[number_of_vertics];
 		for(int j = 0; j < number_of_vertics; j++)
-			init_pher[i][j] = 0;
+			init_pher[i][j] = graph[i][j];
 	}
  
 	return init_pher;
@@ -72,7 +73,7 @@ double *init_flow(bool is_forward){
 
 
 /* Normalize the pheromean*/
-int normalize(double **pher, bool is_row){
+double **normalize(double **pher, bool is_row){
 	double **normalize_pher = new double*[number_of_vertics];
 	for (int i = 0; i < number_of_vertics; i++){
 		normalize_pher[i] = new double[number_of_vertics];
@@ -99,14 +100,72 @@ int normalize(double **pher, bool is_row){
 			}
 		}
 	}
+	return normalize_pher;
 }
+
+/* Update the pheromean*/
+double **update(double **pher, double **forward_flow_matrix, double **backward_flow_matrix){
+	for (int i = 0; i < number_of_vertics; i++){
+		for(int j = 0; j < number_of_vertics; j++){
+			pher[i][j] = decay * pher[i][j] * forward_flow_matrix[i][j] * backward_flow_matrix[i][j];
+		}
+	}	
+}
+
+/* Flow matrix calculation */ 
+double **flow_matrix(double **matrix, double *vertor, bool is_col){
+	double **result = new double*[number_of_vertics];
+	for (int i = 0; i < number_of_vertics; i++){
+		result[i] = new double[number_of_vertics];
+	}
+	if(is_col){
+		for (int i = 0; i < number_of_vertics; i++){
+			for (int j = 0; j < number_of_vertics; j++){
+				result[i][j] = matrix[i][j] * vertor[i];
+			}
+		}
+	}
+	else{
+		for (int j = 0; j < number_of_vertics; j++){
+			for (int i = 0; i < number_of_vertics; i++){
+				result[i][j] = matrix[i][j] * vertor[j];
+			}
+		}
+	}
+	return result;
+}
+
+/* New flow calculation */ 
+double *new_flow(double **matrix, bool is_col){
+	double *result = new double[number_of_vertics];
+	if(is_col){
+		for (int i = 0; i < number_of_vertics; i++){
+			double sum = 0;
+			for (int j = 0; j < number_of_vertics; j++){
+				sum += matrix[i][j];
+			}
+			result[i] = sum * (1 - leakage_val);
+		}
+	}
+	else{
+		for (int j = 0; j < number_of_vertics; j++){
+			double sum = 0;
+			for (int i = 0; i < number_of_vertics; i++){
+				sum += matrix[i][j];
+			}
+			result[j] = sum * (1 - leakage_val);
+		}
+	}
+	return result;
+}
+
 
 
 /* Main */
 int main() {
     double **graph = load_graph();
     cout << "-----------load graph, done-----------\n";
-	double **pher = init_pher();
+	double **pher = init_pher(graph);
 	cout << "-----------initialize the phermone, done-----------\n";
 	double *leakage = init_leakage();
 	cout << "-----------initialize the leakage, done-----------\n";
@@ -119,16 +178,32 @@ int main() {
 		// normalize the pheromone
 		double **norm_pher_forward =  normalize(pher, true);
 		double **norm_pher_backward =  normalize(pher, false);
-		
+			
 		// update pheromone
-		
+		double **forward_flow_matrix = flow_matrix(norm_pher_forward, forward_flow, true);
+		double **backward_flow_matrix = flow_matrix(norm_pher_backward, backward_flow, false);
+		update(pher, forward_flow_matrix, norm_pher_backward);
+
 		
 		// updtae forward_flow and backward_flow
+		double *forward_flow = new_flow(forward_flow_matrix, true);
+		double *backward_flow = new_flow(backward_flow_matrix, false);
+		forward_flow[0] = start_flow * inc_rate;
+		backward_flow[number_of_vertics-1] = end_flow * inc_rate;
 		
+		// next iteration
+		iter++;
+	}
+	
+	cout << "-----------Final pheromone matrix-----------\n";
+	for (int i = 0; i < number_of_vertics; i++){
+		for (int j = 0; j < number_of_vertics; j++){
+			cout << pher[i][j];
+		}
+		cout << "\n";
 	}
 	
     return 0;
 }
-
 
 
